3c015a883a33c08c807c1d774832829d13ff33a1
Johnnycheck <johnnycheck1234@gmail.com>
2024-07-09 13:09:09 -0500

    Initial commit

---
+++ src/main/java/net/mayavampire/neworesplusreworked/NewOresPlusReworkedDataGenerator.java
@@ -0,0 +1,11 @@
+package net.mayavampire.neworesplusreworked;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class NewOresPlusReworkedDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
